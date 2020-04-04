42e1f75eb772f176ffb6cd7372375ab82fda30ef
Khalak97 <valodyakh@gmail.com>
2020-04-04 14:00:07 +0300

    add navbar date dropdown

---
+++ src/layouts/MainLayout.vue
@@ -0,0 +1,35 @@
+<template>
+    <div>
+        <Navbar @clickNav="isOpen = !isOpen" />
+        <Sidebar v-model="isOpen" />
+
+        <div class="app-main-layout">
+            <main class="app-content" :class="{full: !isOpen}">
+                <div class="app-page">
+                    <router-view />
+                </div>
+            </main>
+            <div class="fixed-action-btn">
+                <router-link class="btn-floating btn-large blue" to="/record">
+                    <i class="large material-icons">add</i>
+                </router-link>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+import Navbar from '../components/Navbar'
+import Sidebar from '../components/Sidebar'
+
+export default {
+    name: 'main-layout',
+    data: () => ({
+        isOpen: "true"
+    }),
+    components: {
+        Navbar,
+        Sidebar
+    }
+}
+</script>
